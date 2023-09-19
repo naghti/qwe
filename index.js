fd564846edb58204d9aa38c8ada8116c0aef6231
Your Name <you@example.com>
2023-09-19 18:21:18 +0700

    bruh

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 
-const PORT = 5000 || config.get('serverPort')
+const PORT = process.env.PORT || 3000;
+
 
 const app = express()
 
@@ -8,7 +9,9 @@ app.use(express.json())
 
 app.post('/',(req,res) => {
     console.log(req.body)
-    res.status(200).json('Сервер рабоDWтае1т')
+
+    setTimeout(() => {res.status(200).json('Сервер рабоDWтае1т')}, 7000);
+
 
 })
 
