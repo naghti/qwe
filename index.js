18bc8a07417c75e28adadd8273ebef5ad5522e8c
naghti <57380294+naghti@users.noreply.github.com>
2023-09-19 18:05:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ app.use(express.json())
 
 app.post('/',(req,res) => {
     console.log(req.body)
-    res.status(200).json('Сервер рабоDWтае1т')
+    
+    setTimeout(() => {res.status(200).json('Сервер рабоDWтае1т')}, 1000);
+    
 
 })
 
